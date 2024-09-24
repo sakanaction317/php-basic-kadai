7a4db3df7153acb8d66d9042d765caa6ae0dd94c
sakanaction317 <danki.karate9602@gmail.com>
2024-09-24 22:46:30 +0900

    kadai_016をアップロード

---
+++ kadai_016/class.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+    <p>
+        <?php
+        class Food{
+            // プロパティ
+            public $name;
+            public $price;
+
+            // メソッド
+            public function show_price(int $price){
+                $this->price = $price;
+                echo $this->price . '<br>';
+            }
+
+            // コンストラクタ
+            public function __construct(string $name, int $price){
+                $this->name = $name;
+                $this->price = $price;
+            }
+        }
+
+        class Animal{
+            // プロパティ
+            public $name;
+            public $height;
+            public $weight;
+
+            // メソッド
+            public function show_height(int $height){
+                $this->height = $height;
+                echo $this->height . '<br>';
+            }
+
+            // コンストラクタ
+            public function __construct(string $name, int $price, int $height){
+                $this->name = $name;
+                $this->price = $price;
+                $this->height = $height;
+            }
+        }
+
+        // インスタンスを出力
+        $apple = new Food('りんご', 300);
+        print_r($apple);
+
+        echo '<br>';
+
+        $dog = new Animal('犬', 80, 70);
+        print_r($dog);
+
+        echo '<br>';
+
+        // メソッドにアクセス
+        $apple->show_price(300);
+        $dog->show_height(70);
+        ?>
+    </p>
+ </body>
+ 
+ </html>
